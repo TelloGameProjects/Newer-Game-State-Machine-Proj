9d4acfc88a2e9c980e806f392ed8edcdfa16ca54
Tello- <tellovisionstudio@gmail.com>
2021-08-07 21:33:55 -0700

    Main Menu now works with start button

---
+++ Assets/UIHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Unity​Engine.UIElements;
+
+
+public class UIHandler : MonoBehaviour
+{
+    // provide reference in editor inspector (from step 2)
+    public UIDocument UIDocument;
+    Button button;
+    Label label;
+    TextField textField;
+    void Start()
+    {
+        var root = UIDocument.rootVisualElement;
+        // get ui elements by name
+        button = root.Q<Button>("start-button");
+
+        // add event handler
+        button.clickable.clicked += Button_clicked;
+    }
+
+    private void Button_clicked()
+    {
+        Debug.Log("Start Button Clicked");
+    }
+}
