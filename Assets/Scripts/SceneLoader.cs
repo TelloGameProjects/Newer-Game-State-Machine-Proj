97e11969f7589ab4043e513b5e229c22c39014a0
Tello- <tellovisionstudio@gmail.com>
2021-08-11 15:08:40 -0700

    Creating Scene Loader class

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneLoader
+{
+
+    public enum Scene           // Note: Spelling of enum fields matter
+    {
+        MainMenuScene, 
+        PlayingScene, 
+        PausingScene, 
+        GameOverScene
+    }
+}
