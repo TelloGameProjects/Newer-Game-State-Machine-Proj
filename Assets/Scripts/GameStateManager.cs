8e3db66aa5f8ef68b86852110a7c98c319b23210
Tello- <tellovisionstudio@gmail.com>
2021-08-12 13:26:02 -0700

    Cleared out file
    Not using right now but don't want to get rid of

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -6,11 +6,6 @@ public class GameStateManager : MonoSingleton<GameStateManager>
 {
     [SerializeField] private AppManager stateMachine;
 
-    [SerializeField] private GameObject mainMenuManager;
-    [SerializeField] private GameObject playingManager;
-    [SerializeField] private GameObject pauseManager;
-    [SerializeField] private GameObject gameOverManager;
-
     [SerializeField] private MainMenuStateAsset mainMenuState;
     [SerializeField] private PlayingStateAsset playingState;
     [SerializeField] private PauseStateAsset pauseState;
@@ -23,13 +18,6 @@ public class GameStateManager : MonoSingleton<GameStateManager>
 
     private void Awake()
     {
-        //Debug.Log("SceneInitializer.Awake() called");
-        if (mainMenuManager.tag == "MainMenuManager")
-        {
-            mainMenuManager.SetActive(true);
-            
-           // Debug.Log("mainMenuManager.SetActive(true);");
-        }
     }
 
 
@@ -37,22 +25,6 @@ public class GameStateManager : MonoSingleton<GameStateManager>
     void Start()
     {
 
-
-        if (mainMenuState.StateObject != null)
-        {
-            
-                
-            if(StateMachine.AppStateMachine.ContainsState(mainMenuState.StateObject))
-            {
-                //Debug.Log("stateMachine DOES contain mainMenuState.StateObject");
-                StateMachine.AppStateMachine.ChangeState(mainMenuState.StateObject);
-                MainMenuStartButton.StartButtonControl.clickable.clicked += TransitionToPlayState;
-            }
-            else
-            {
-                Debug.Log("stateMachine DOES NOT contain mainMenuState.StateObject");
-            }
-        }
     }
 
     // Update is called once per frame
@@ -61,20 +33,4 @@ public class GameStateManager : MonoSingleton<GameStateManager>
         
     }
 
-    void TransitionToPlayState()
-    {
-        playingManager.SetActive(true);
-        if (StateMachine.AppStateMachine.ContainsState(playingState.StateObject))
-        {
-            //Debug.Log("StateMachine DOES contain playing state... transitioning now.");
-            StateMachine.AppStateMachine.ChangeState(playingState.StateObject);
-            mainMenuManager.SetActive(false);
-        }
-        else
-        {
-            //Debug.Log("StateMachine DOES NOT contain playing state... not transitioning now.");
-            playingManager.SetActive(false);
-        }
-     
-    }
 }
