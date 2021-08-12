24df6dd45c9f60cf2ff26b7fdd90509f6c17bab8
Tello- <tellovisionstudio@gmail.com>
2021-08-12 13:37:55 -0700

    Exit button should now print debug text

--- Assets/Scripts/ExitButtonResponse.cs
+++ Assets/Scripts/ExitButtonResponse.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ExitButtonResponse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void RespondToExitButton()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Exit button clicked");
     }
 }
